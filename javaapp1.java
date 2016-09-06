04bc33ef6e8f9bc00447d05494e11e3b1ea257a0
Michael Graves <sevargekim@gmail.com>
2016-09-06 19:54:31 -0400

    Create javaapp1.java

---
+++ javaapp1.java
@@ -0,0 +1,20 @@
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String myName = "Michael Graves";
+        String myPurpose = "Become a professional software engineer.";
+        String myFuture = "Automate the world.";
+        
+        System.out.print("\tName: "); // /t is a tab, okay to run in with text
+        System.out.println(myName);
+        System.out.print("Purpose: ");
+        System.out.println(myPurpose);
+        System.out.print("Future: ");
+        System.out.println(myFuture);
+    }
+    
+}
