8076910901aff4dbe1c7bbe7528c513c5eaabd4e
Michael Graves <sevargekim@gmail.com>
2016-09-28 10:56:47 -0400

    Add files via upload

---
+++ MPGCalc.java
@@ -0,0 +1,25 @@
+import javax.swing.JOptionPane;
+
+public class MPGCalc {
+    public static void main(String[] args) {
+        int numMilesDriven;
+        String milesDriven;
+        String gasUsed;
+        int numGasUsed;
+        int mpg;
+        
+        milesDriven = JOptionPane.showInputDialog("How many miles have you"
+                + " driven?");
+        
+        numMilesDriven = Integer.parseInt(milesDriven);
+        
+        gasUsed = JOptionPane.showInputDialog("How much gas have you used "
+                + "in gallons?");
+        numGasUsed = Integer.parseInt(gasUsed);
+        
+        mpg = numMilesDriven / numGasUsed;
+        
+        JOptionPane.showMessageDialog(null, "Your MPG is " + mpg + ".");
+        
+    }
+}
