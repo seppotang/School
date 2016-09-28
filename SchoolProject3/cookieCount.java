be0b913a7e9bbd4f650fab994c5a68da6d363dbd
Michael Graves <sevargekim@gmail.com>
2016-09-28 10:46:03 -0400

    Update cookieCount.java

--- SchoolProject3/cookieCount.java
+++ SchoolProject3/cookieCount.java
@@ -1,1 +1,33 @@
+import java.util.Scanner;
 
+public class cookieCount {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner keyboard = new Scanner(System.in);
+        
+        int cookieCount;
+        double cupsSugar;
+        int cupsButter;
+        double cupsFlour;
+        
+        System.out.println("How many cookies are desired?");
+        cookieCount=keyboard.nextInt();
+        
+        cupsSugar = 1.5*(cookieCount/48);
+        cupsButter = 1*(cookieCount/48);
+        cupsFlour = 2.75*(cookieCount/48);
+        
+        System.out.println(cupsSugar);
+        System.out.println(cupsButter);
+        System.out.println(cupsFlour);
+        
+        
+        
+        
+    }
+    
+}
