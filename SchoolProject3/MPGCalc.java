1d020460bb4fa9b03d0577ffd5cde2e146f68ac3
Michael Graves <sevargekim@gmail.com>
2016-09-28 10:47:01 -0400

    Update MPGCalc.java

--- SchoolProject3/MPGCalc.java
+++ SchoolProject3/MPGCalc.java
@@ -1,1 +1,23 @@
-
+public class MPGCalc {
+    public static void main(String[] args) {
+        int numMilesDriven;
+        String milesDriven;
+        String gasUsed;
+        int numGasUsed;
+        int mpg;
+        
+        milesDriven = JOptionPane.showInputDialog("How many miles have you"
+                + " driven?");
+        
+        numMilesDriven = Integer.parseInt(milesDriven);
+        
+        gasUsed = JOptionPane.showInputDialog("How much gas have you used "
+                + "in gallons?");
+        numGasUsed = Integer.parseInt(gasUsed);
+        
+        mpg = numMilesDriven / numGasUsed;
+        
+        JOptionPane.showMessageDialog(null, "Your MPG is " + mpg + ".");
+        
+    }
+}
