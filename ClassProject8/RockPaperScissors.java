be69f46949ef662c7111407741414edb9a6dd62f
Michael Graves <mjgraves91@gmail.com>
2016-11-15 16:20:51 -0500

    Create RockPaperScissors.java

---
+++ ClassProject8/RockPaperScissors.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class RockPaperScissors {
+	public static void main(String[] args) {
+		int compNum, userNum;
+		
+		compNum = Math.random
+	}
+}
+
+public static int randInt(int min, int max) {
+	int randomNum = rand.nextInt(3) + 1;
+	
+	return randomNum;
+}
