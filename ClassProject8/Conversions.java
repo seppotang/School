68dfa866fcf2c993e865b2e57f457267cd6aea64
Michael Graves <mjgraves91@gmail.com>
2016-11-23 14:11:15 -0500

    Update Conversions.java

--- ClassProject8/Conversions.java
+++ ClassProject8/Conversions.java
@@ -1,62 +1,67 @@
-import java.util.*;
+import java.util.Scanner; 
 
 public class Conversions {
-	public static void main(String[] args) {
-		Scanner keyboard = new Scanner(System.in);
-		int distanceM, distanceKM, distanceINCH, distanceFT;
-		
-		do {
-		System.out.println("Please enter a distance in meters.");
-		while(!keyboard.nextHasInt()){
-			System.out.println("That's not a number!");
-			System.out.println("Please enter a distance in meters.");
-			keyboard.next();
-		}
-		distanceM = keyboard.nextInt();
-			if(distanceM <= 0) {
-				System.out.println("Please enter a positive number.");
-			}
-		} while(distanceM <= 0);
-	}
-	
-	public void showKilometers(distanceM) {
-		distanceKM = distanceM * .001;
-		System.out.println(distanceKM);
-	}
+    
+//Initialize constructors before main
+public static void showKilometers(double meters) //Convert to Kilos
+{ 
+double kilometers = meters * 0.001; 
+System.out.println(meters +" meters is " + kilometers + " kilometers."); 
+} 
 
-	public void showInches(distanceM) {
-		distanceINCH = distanceM * 39.27;
-		System.out.println(distanceINCH);
-	}
+public static void showInches(double meters) //Convert to Inches
+{ 
+double inches = meters * 39.37; 
+System.out.println(meters +" meters is " + inches + " inches."); 
+} 
 
-	public void showFeet(distanceM) {
-		distanceFT = distanceM * 3.281;
-		System.out.println(distanceFT);
-	}
+public static void showFeet(double meters) //Convert to feet
+{ 
+double feet = meters * 3.281; 
+System.out.println(meters +" meters is " + feet + " feet."); 
+} 
 
-	public void menu() {
-		System.out.println("1. Convert Kilometers");
-		System.out.println("2. Convert Inches");
-		System.out.println("3. Convert Feet");
-		System.out.println("Please choose an option.");
-	
-		int selection = keyboard.nextInt();
-	
-		switch(selection) {
-			case 1:
-				showFeet(distanceM);
-				break;
-			case 2:
-				showInches(distanceM);
-				break;
-			case 3:
-				showFeet(distanceM);
-				break;
-			default:
-				printf("Not an option. Please choose again.");
-		}	
-		
-	}
-	
-}
+public static void quitProgram() 
+{ 
+System.out.println("Goodbye!"); 
+System.out.println(0); 
+} 
+public static void showMenu() 
+{ 
+System.out.println(" 1. Convert to kilometers "); 
+System.out.println(" 2. Convert to inches "); 
+System.out.println(" 3. Convert to feet "); 
+System.out.println(" 4. Quit the program "); 
+System.out.println(" "); 
+} 
 
+//Define main function
+public static void main (String [] args) 
+{ 
+
+double meters; 
+int choice; 
+
+
+
+Scanner keyboard = new Scanner (System.in); 
+
+System.out.println("Enter a distance in meters: "); 
+meters = keyboard.nextDouble(); 
+showMenu(); 
+choice = keyboard.nextInt(); 
+switch(choice) { 
+    case 1: 
+        showKilometers(meters); 
+        break; 
+    case 2:
+        showInches(meters); 
+        break; 
+    case 3:
+        showFeet(meters); 
+        break; 
+    case 4: 
+    quitProgram(); 
+        } 
+    } 
+} 
